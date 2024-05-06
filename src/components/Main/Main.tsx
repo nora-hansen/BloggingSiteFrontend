194834e1c6b56e5aba62dc366a178422a2c2da44
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-06 12:56:12 +0200

    Set height of app so that, hopefully. if the main part grows large, the app will grow its height

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,7 +4,11 @@ function Main()
 {
     return(
         <main>
+            <h1>HOME</h1>
             <h1>Soups</h1>
+            <p>Lorem ipsum and the like, hello. This is the main part of the website. Stuff will be here. With time..</p>
+            <p>This is technically the first update that will show, so, uh, actually, nvm</p>
+            <p>Check in later for a coooooool website</p>
         </main>
     );
 }
