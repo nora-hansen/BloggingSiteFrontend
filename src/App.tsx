22a4ad2eaef139d5dbbe0dd883c5a3be31630029
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-06 12:43:56 +0200

    Created Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import TopBar from './components/TopBar/TopBar'
+import Main from './components/Main/Main'
 
 function App() {
   return (
-    <>
-    <TopBar />
-    </>
+    <div className="blog-fe">
+      <TopBar />
+      <Main />
+    </div>
   )
 }
 
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import './Main.css';
+
+function Main()
+{
+    return(
+        <main>
+            <h1>Soups</h1>
+        </main>
+    );
+}
+
+export default Main;
