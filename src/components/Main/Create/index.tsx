4a6bfc7bd82c2ed34e9dc98d0011a4a682e8048a
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-08 09:19:36 +0200

    Input fields are in place

--- src/components/Main/Create/index.tsx
+++ src/components/Main/Create/index.tsx
@@ -2,17 +2,40 @@ import './Create.css'
 
 function Create()
 {
+    const handleChange = (event) => {
+        const inputValue = event.target.value
+        const name = event.target.name
+    }
+
     return(
         <div className="create-post-page">
             <h1>Create new post</h1>
-            <div>
-                <label htmlFor="title">Title</label>
-                <input type="text" name="title" placeholder="Title (optional)"></input>
-                <label htmlFor="content" ></label>
-                <textarea id="content" placeholder="What's on your mind?">
+            <form>
+                <div className="create-post-input-section">
+                    <input type="text" name="title" placeholder="Title (optional)"/>
+                    <label htmlFor="content" ></label>
+                    <textarea id="content" name="content" placeholder="What's on your mind?">
+                    </textarea>
+                </div>
+                <div className="create-post-info-edit">
+                        <fieldset>
+                        <legend>Visibility</legend>
+                            <div className="create-post-visibility-radio">
+                                <input type="radio" id="Public" name="visibility" value="public"/> 
+                                <label htmlFor="Public">Public</label>
+                                <input type="radio" id="FriendsOnly" name="visibility" value="friendsonly"/>                     
+                                <label htmlFor="FriendsOnly">Friends only</label>
+                                <input type="radio" id="Private" name="visibility" value="private"/> 
+                                <label htmlFor="Private">Private</label>
+                            </div>
+                        </fieldset>
+                </div>
+                <div className="create-post-buttons">
+                    <button>Save as draft</button>
+                    <button>Post</button>
+                </div>
+            </form>
 
-                </textarea>
-            </div>
         </div>
     )
 }
