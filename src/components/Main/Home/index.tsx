228ac7df9ebf7e51e3466738aca03c5a403b6618
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-08 14:01:37 +0200

    I don't know what to put on the home page yet

--- src/components/Main/Home/index.tsx
+++ src/components/Main/Home/index.tsx
@@ -1,9 +1,19 @@
+import './Home.css'
+
 function Home()
 {
     return(
-        <>
+        <div className="home">
             <h1>Home</h1>
-        </>
+            <div className="home-updates">
+                <h1>What's new?</h1>
+                <div className="home-updates-list">
+                    <ul>
+                        <li>- The beginning of the home page</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
