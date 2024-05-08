4bd75878bc9ed6e0a8a30d54de4bc20c3998ccb2
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-08 13:51:48 +0200

    Added Color preview

--- src/components/Main/EditProfile/index.tsx
+++ src/components/Main/EditProfile/index.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react'
 import './EditProfile.css'
+
+interface IStyle {
+    bgColor: string,
+    fontColor: string,
+    postColor: string
+}
 
 function EditProfile()
 {
+    const [style, setStyle] = useState<IStyle>({
+        bgColor: "#aaaaaa",
+        fontColor: "#000000",
+        postColor: "#FFFFFF"
+    })
+
     const handleSubmit = () => {
         alert("Thank you! Om nom")
+    }
+
+    const handleChange = (event) => {
+        const inputValue = event.target.value
+        const name = event.target.name
+
+        if (name === "bgColor") setStyle({...style, bgColor: inputValue})
+        if (name === "fontColor") setStyle({...style, fontColor: inputValue})
+        if (name === "postColor") setStyle({...style, postColor: inputValue})
     }
 
     return(
@@ -13,15 +35,15 @@ function EditProfile()
                 <div className="profile-colors">
                     <label htmlFor="bgColor">
                         Background color
-                        <input type="text" placeholder="#FFFFFF"></input>
+                        <input type="text" name="bgColor" placeholder="#aaaaaa" onChange={handleChange}></input>
                     </label>
                     <label htmlFor="fontColor">
                         Font color
-                        <input type="text" placeholder="#000000"></input>
+                        <input type="text" name="fontColor" placeholder="#000000" onChange={handleChange}></input>
                     </label>
                     <label htmlFor="postColor">
                         Post color
-                        <input type="text" placeholder="#FFFFFF"></input>
+                        <input type="text" name="postColor" placeholder="#FFFFFF" onChange={handleChange}></input>
                     </label>
                 </div>
                 <div className="profile-bio-edit">
@@ -29,7 +51,20 @@ function EditProfile()
                         <textarea placeholder="Bio (This should hold the user's existing bio)"></textarea>
                     </label>
                 </div>
+                <button>Save changes</button>
             </form>
+            <h1>Color preview</h1>
+            <div className="profile-preview" 
+            style={{backgroundColor: style.bgColor ? style.bgColor : "#aaaaaa"}}
+            >
+                <div className="profile-preview-post"
+                style={{backgroundColor: style.postColor ? style.postColor : "#FFFFFF",
+                color: style.fontColor ? style.fontColor : "#000000"
+                }}>
+                    <h1>Lorem Ipsum</h1>
+                    <p>Dolor sit amet, , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
         </div>
     )
 }
