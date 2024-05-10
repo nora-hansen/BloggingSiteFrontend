faceabbefeeca61e86bd6ed86e5aca445a473949
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-10 15:24:35 +0200

    Minor styling

--- src/components/Main/Users/UsersList/UsersListItem/index.tsx
+++ src/components/Main/Users/UsersList/UsersListItem/index.tsx
@@ -1,9 +1,11 @@
 import './UsersListItem.css'
 
-function UsersListItem() {
+function UsersListItem(user: {iconUrl: string, displayName: string, id: number}) {
     return(
-        <>
-        </>
+        <div className="users-list-item">
+            <img src={user.iconUrl ? user.iconUrl : "../hamster.jpg"} alt={`${user.displayName}'s profile image`}></img>
+            <h2>{user.displayName}</h2>
+        </div>
     )
 }
 
--- src/components/Main/Users/UsersList/index.tsx
+++ src/components/Main/Users/UsersList/index.tsx
@@ -1,9 +1,33 @@
 import './UsersList.css'
+import env from '../../../../environment'
+import { useEffect, useState } from 'react'
+
+import { IUser } from '../../All/Post'
+import UsersListItem from './UsersListItem'
 
 function UsersList() {
+    const [users, setUsers] = useState<IUser[]>([])
+    useEffect(() => {
+        fetch(`${env.url}/users`)
+        .then(response => response.json())
+        .then(data => setUsers(data))
+    }, [])
+
+
+    if(!users) 
+        return <p>Loading...</p>
+
     return(
-        <>
-        </>
+        <div className="users-list">
+            {users.map((user, index) => 
+                <UsersListItem 
+                    key={index}
+                    displayName={user.displayName}
+                    iconUrl={user.iconUrl}
+                    id={user.id}
+                />
+            )}
+        </div>
     )
 }
 
