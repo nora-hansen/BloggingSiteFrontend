cea6631125ad3934c228098ab88f9e0fa6b496de
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-10 15:28:22 +0200

    User list links to user profiles

--- src/components/Main/Users/UsersList/UsersListItem/index.tsx
+++ src/components/Main/Users/UsersList/UsersListItem/index.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 import './UsersListItem.css'
+import env from '../../../../../environment'
 
 function UsersListItem(user: {iconUrl: string, displayName: string, id: number}) {
     return(
         <div className="users-list-item">
             <img src={user.iconUrl ? user.iconUrl : "../hamster.jpg"} alt={`${user.displayName}'s profile image`}></img>
-            <h2>{user.displayName}</h2>
+            <Link to={`/user/${user.id}`}><h2>{user.displayName}</h2></Link>
         </div>
     )
 }
