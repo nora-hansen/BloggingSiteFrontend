db421473259a4d5bc350157e3d8e2b7c15af985d
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-15 14:37:48 +0200

    Removed unused ts files

--- src/components/Main/Post/index.tsx
+++ src/components/Main/Post/index.tsx
@@ -1,22 +1,38 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './Post.css'
 import { useEffect, useState } from 'react';
 import env from '../../../environment';
 import { IPost } from '../../../App';
+import { IUser } from '../All/Post';
 
 function Post() {
     const { postId } = useParams<{ postId?: string }>();
     const [post, setPost] = useState<IPost>()
+    const [user, setUser] = useState<IUser>()
 
     useEffect(() => {
         fetch(`${env.url}/posts/${postId}`)
             .then(response => response.json())
             .then(data => setPost(data))
-    })
+    }, [])
+
+    useEffect(() => {
+        fetch(`${env.url}/users/${post?.userID}`)
+            .then(response => response.json())
+            .then(data => setUser(data))
+    }, [post])
 
     return(
         <div className="single-post">
-            <h1>{post?.title}</h1>
+            <div className="user-info">
+                <img src={user?.iconUrl}></img>
+                <Link to={`/user/${user?.id}`}><p>{user?.displayName}</p></Link>
+            </div>
+            <div className="single-post-content">
+                <h1>{post?.title}</h1>
+                <p>{post?.content}</p>
+            </div>
+            <p></p>
         </div>
     )
 }
