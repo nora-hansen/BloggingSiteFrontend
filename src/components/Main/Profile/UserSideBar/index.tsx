92e3e998413afa665d85840a6deee037af21a0c6
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-16 15:18:03 +0200

    Removed some unused imports

--- src/components/Main/Profile/UserSideBar/index.tsx
+++ src/components/Main/Profile/UserSideBar/index.tsx
@@ -1,7 +1,4 @@
-import { useContext } from 'react'
 import './UserSideBar.css'
-import { UserContext } from '../../../../App'
-import { Link } from 'react-router-dom'
 import Bio from './Bio'
 import UserImageName from './UserImageName'
 import Buttons from './Buttons'
@@ -10,8 +7,6 @@ function UserSideBar(
         user: {iconUrl: string, displayName: string, bio: string, postColor: string, fontColor: string,
         userId: number}
     ) {
-    const userContext = useContext(UserContext)
-
     return(
         <div className='profile-user-details'>
                 <UserImageName displayName={user.displayName} iconUrl={user.iconUrl} />
