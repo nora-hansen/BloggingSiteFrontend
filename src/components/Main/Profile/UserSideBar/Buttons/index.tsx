3b5ea44cacce1899a1bc73bd635b6219ee9e2e15
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-16 15:32:03 +0200

    Added a check when going to user profiles of whether or not a friend request was sent

--- src/components/Main/Profile/UserSideBar/Buttons/index.tsx
+++ src/components/Main/Profile/UserSideBar/Buttons/index.tsx
@@ -1,14 +1,33 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './Buttons.css'
 import { UserContext } from '../../../../../App'
 import { Link } from 'react-router-dom'
+import env from '../../../../../environment'
 
 function Buttons(user: {userId: number}) {
     const userContext = useContext(UserContext)
+    const [activeFriendRequest, setActiveFriendRequest] = useState<boolean>()
+
+    useEffect(() => {
+        if (user?.userId !== userContext.user?.id)
+        {
+            fetch(`${env.url}/friendrequests?senderId=${userContext.user?.id}&recipientId=${user?.userId}`)
+            .then(response => {
+                if(!response.ok) throw new Error(`${response.status}`)
+                else return response.json()
+            })
+            .then(data => setActiveFriendRequest(data))
+            .catch((error) => {
+                console.log(error)
+            })
+        }
+    }, [user?.userId, userContext.user?.id])
+
     return(
         <>
             {Number(user?.userId) == userContext.user?.id && <Link to="/edit-profile"><button>Edit profile</button></Link>}
-            {Number(user?.userId) != userContext.user?.id && userContext.bearer != "" && <button>Add friend</button>}
+            {Number(user?.userId) != userContext.user?.id && userContext.bearer != "" && !activeFriendRequest && <button>Add friend</button>}
+            {Number(user?.userId) != userContext.user?.id && userContext.bearer != "" && activeFriendRequest && <p>Friend request sent!</p>}
         </>
     )
 }
