3294ff8a3e66875901c63d0b0db5be2f27541604
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-05-27 12:54:25 +0200

    Starting on loader

--- src/Loader.ts
+++ src/Loader.ts
@@ -1,3 +1,11 @@
 import { json } from "react-router-dom";
 import type { LoaderFunctionArgs } from "react-router-dom";
+import env from "./environment";
 
+const POSTS = []
+
+export const loader = () => {
+    fetch(`${env.url}/posts`)
+    .then(response => response.json())
+    .then(data => setPosts(data))
+}
